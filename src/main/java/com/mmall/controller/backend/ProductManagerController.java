19ff99cbde8c21de4784fa82511dcc4fe3380b49
sww <sww_603@163.com>
2018-09-28 18:08:24 +0800

    ftp接口

--- src/main/java/com/mmall/controller/backend/ProductManagerController.java
+++ src/main/java/com/mmall/controller/backend/ProductManagerController.java
@@ -1,5 +1,6 @@
 package com.mmall.controller.backend;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
@@ -9,8 +10,12 @@ import com.mmall.service.FileService;
 import com.mmall.service.IUserService;
 import com.mmall.service.ProductService;
 import com.mmall.util.PropertiesUtil;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +27,8 @@ import org.springframework.web.multipart.MultipartFile;
  * Created by sww_6 on 2018/9/6.
  */
 @Controller
-@RequestMapping("/manager/product")
+@RequestMapping("/manage/product")
+@Slf4j
 public class ProductManagerController {
 
   @Autowired
@@ -117,10 +123,59 @@ public class ProductManagerController {
 
   @RequestMapping("upload.do")
   @ResponseBody
-  public ServerResponse upload(MultipartFile file, HttpServletRequest request) {
+  public ServerResponse upload(HttpSession session,
+      @RequestParam(value = "upload_file", required = false) MultipartFile file,
+      HttpServletRequest request) {
+    User user = (User) session.getAttribute(Const.CURRENT_USER);
+
     String path = request.getSession().getServletContext().getRealPath("upload");
-    String upload = fileService.upload(file, path);
-    String stringUrl = PropertiesUtil.getProperty("");
-    return null;
+    String targetFileName = fileService.upload(file, path);
+    String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+    Map fileMap = Maps.newHashMap();
+    fileMap.put("uri", targetFileName);
+    fileMap.put("url", url);
+    return ServerResponse.createBySuccess(fileMap);
+  }
+
+  @RequestMapping("richtext_img_upload.do")
+  @ResponseBody
+  public Map richtextImgUpload(HttpSession session,
+      @RequestParam(value = "upload_file", required = false) MultipartFile file,
+      HttpServletRequest request, HttpServletResponse response) {
+    Map resultMap = Maps.newHashMap();
+    log.info(resultMap.toString());
+    User user = (User) session.getAttribute(Const.CURRENT_USER);
+      /* if (user == null) {
+            resultMap.put("success", false);
+            resultMap.put("msg", "请登录管理员");
+            return resultMap;
+        }*/
+    //富文本中对于返回值有自己的要求,我们使用是simditor所以按照simditor的要求进行返回
+//        {
+//            "success": true/false,
+//                "msg": "error message", # optional
+//            "file_path": "[real file path]"
+//        }
+    //if (iUserService.checkAdminRole(user).isSuccess()) {
+    String path = request.getSession().getServletContext().getRealPath("hahaha");
+    String targetFileName = fileService.upload(file, path);
+    if (StringUtils.isBlank(targetFileName)) {
+      resultMap.put("success", false);
+      resultMap.put("msg", "上传失败");
+      return resultMap;
+
+    }
+    String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+    resultMap.put("success", true);
+    resultMap.put("msg", "上传成功");
+    resultMap.put("file_path", url);
+    response.addHeader("Access-Control-Allow-Headers", "X-File-Name");
+    return resultMap;
+        /*} else {
+            resultMap.put("success", false);
+            resultMap.put("msg", "无权限操作");
+            return resultMap;
+        }*/
   }
 }
