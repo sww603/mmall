5c2d6047df9a9cce7d365188a48f57afef43d6f6
sww <sww_603@163.com>
2018-10-12 17:55:58 +0800

    购物车添加功能

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -14,8 +14,13 @@ public class Const {
     int ROLE_CUSTOMER = 0; //普通用户
     int ROLE_ADMIN = 1;//管理员
   }
-  public interface cart{
+
+  public interface cart {
+
     int CHENCKED = 1;//被选中
     int UN_CHENCKED = 0;//未被选中
+
+    String LIMIT_NUM_FATL = "LIMIT_NUM_FATL";
+    String LIMIT_NUM_SUCESS = "LIMIT_NUM_SUCESS";
   }
 }
--- src/main/java/com/mmall/service/impl/CartServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CartServiceImpl.java
@@ -2,10 +2,15 @@ package com.mmall.service.impl;
 
 import com.google.common.collect.Lists;
 import com.mmall.common.Const;
+import com.mmall.common.Const.cart;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.CartMapper;
+import com.mmall.dao.ProductMapper;
 import com.mmall.pojo.Cart;
+import com.mmall.pojo.Product;
 import com.mmall.service.ICartService;
+import com.mmall.util.BigDecimalUtil;
+import com.mmall.util.PropertiesUtil;
 import com.mmall.vo.CartProductVo;
 import com.mmall.vo.CartVo;
 import java.math.BigDecimal;
@@ -22,6 +27,8 @@ public class CartServiceImpl implements ICartService {
 
   @Autowired
   private CartMapper cartMapper;
+  @Autowired
+  private ProductMapper productMapper;
 
   @Override
   public ServerResponse add(Integer userId, Integer productId, Integer count) {
@@ -51,19 +58,60 @@ public class CartServiceImpl implements ICartService {
     if (CollectionUtils.isNotEmpty(cartLists)) {
       for (Cart cartItem : cartLists) {
         CartProductVo cartProductVo = new CartProductVo();
-        cartProductVo.setId();
-        cartProductVo.setLimitQuantity();
-        cartProductVo.setProductChecked();
-        cartProductVo.setProductId();
-        cartProductVo.setProductName();
-        cartProductVo.setProductPrice();
-        cartProductVo.setProductStatus();
-        cartProductVo.setProductStock();
-        cartProductVo.setProductSubTitle();
-        cartProductVo.setProductTotalPrice();
-        cartProductVo.setQuantity();
+        cartProductVo.setId(cartItem.getId());
+        cartProductVo.setUserId(userId);
+        cartProductVo.setProductChecked(cartItem.getChecked());
+        cartProductVo.setProductId(cartItem.getProductId());
+        cartProductVo.setQuantity(cartItem.getQuantity());
+        Product product = productMapper.selectByPrimaryKey(cartItem.getProductId());
+
+        if (product != null){
+          cartProductVo.setProductMainImage(product.getMainImage());
+          cartProductVo.setProductName(product.getName());
+          cartProductVo.setProductPrice(product.getPrice());
+          cartProductVo.setProductStatus(product.getStatus());
+          cartProductVo.setProductStock(product.getStock());
+          cartProductVo.setProductSubTitle(product.getSubImages());
+
+          int buyLimitCount = 0;
+          //判断库存
+          if (product.getStock() >= cartItem.getQuantity()) {
+            buyLimitCount = cartItem.getQuantity();
+            cartProductVo.setLimitQuantity(cart.LIMIT_NUM_SUCESS);
+          } else {
+            buyLimitCount = cartItem.getQuantity();
+            cartProductVo.setLimitQuantity(cart.LIMIT_NUM_FATL);
+            //购物车中更新有效库存
+            Cart cartForQuantity = new Cart();
+            cartForQuantity.setId(cartItem.getId());
+            cartForQuantity.setQuantity(buyLimitCount);
+            cartMapper.updateByPrimaryKeySelective(cartForQuantity);
+          }
+          cartProductVo.setQuantity(buyLimitCount);
+          cartProductVo.setProductTotalPrice(BigDecimalUtil
+              .mul(product.getPrice().doubleValue(), cartProductVo.getQuantity().doubleValue()));
+          cartProductVo.setProductChecked(cartItem.getChecked());
+        }
+
+        if (cartItem.getChecked()== cart.CHENCKED){
+          cartTotalPrice = BigDecimalUtil.add(cartTotalPrice.doubleValue(),
+              cartProductVo.getProductTotalPrice().doubleValue());
+        }
+        cartProductVos.add(cartProductVo);
       }
     }
+    cartVo.setCartTotalPrice(cartTotalPrice);
+    cartVo.setCartProductVos(cartProductVos);
+    cartVo.setAllChecked(this.getAllCheckedStatus(userId));
+    cartVo.setImageHost(PropertiesUtil.getProperty("ftp.server.http.prefix"));
     return cartVo;
+  }
+
+  private boolean getAllCheckedStatus(Integer userId) {
+    if (userId == null) {
+      return false;
+    }
+    return cartMapper.selectCartProductCheckedStatusByUserId(userId) == 0;
+
   }
 }
